8c3925f9a8b11ea1934d43c0cad7836af6718c94
Konrad Borowski <xfix@protonmail.com>
2017-08-01 23:02:37 +0200

    Provide documentation for array!

--- array-macro/src/lib.rs
+++ array-macro/src/lib.rs
@@ -9,6 +9,21 @@ pub use array_macro_internal::*;
 
 proc_macro_expr_decl!(#[doc(hidden)] __internal_array! => internal_array_impl);
 
+/// Array constructor macro.
+///
+/// This macro provides a way to repeat the same macro element multiple times
+/// without requiring `Copy` implementation.
+///
+/// # Examples
+///
+/// ```
+/// # #[macro_use]
+/// # extern crate array_macro;
+/// # fn main() {
+/// assert_eq!(array!["string"; 3], ["string", "string", "string"]);
+/// assert_eq!(array![|x| x; 3], [0, 1, 2]);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! array {
     [@INTERNAL $callback:expr; $count:tt] => {
