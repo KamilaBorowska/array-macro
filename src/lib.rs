f919a11d11ee1d06a4c5645c569c553b42824ebd
Konrad Borowski <konrad@borowski.pw>
2017-11-19 22:58:02 +0100

    Move internal part of array macro to a separate macro
    This hopefully will improve error messages.

--- src/lib.rs
+++ src/lib.rs
@@ -20,28 +20,9 @@
 #[doc(hidden)]
 pub extern crate core as __core;
 
-/// Array constructor macro.
-///
-/// This macro provides a way to repeat the same macro element multiple times
-/// without requiring `Copy` implementation.
-///
-/// It's possible to define a callback by starting expression with `|` or `move`. As
-/// every closure is it own unique type, it is not possible to have an array of
-/// closures, so this syntax was reused for creating arrays with known indexes.
-///
-/// # Examples
-///
-/// ```
-/// # #[macro_use]
-/// # extern crate array_macro;
-/// # fn main() {
-/// assert_eq!(array!["string"; 3], ["string", "string", "string"]);
-/// assert_eq!(array![|x| x; 3], [0, 1, 2]);
-/// # }
-/// ```
 #[macro_export]
-macro_rules! array {
-    [@INTERNAL $callback:expr; $count:expr] => {{
+macro_rules! __array_internal {
+    [$callback:expr; $count:expr] => {{
         #[allow(unused_mut)]
         let mut callback = $callback;
         unsafe {
@@ -81,11 +62,34 @@ macro_rules! array {
             ::array_macro::__core::mem::ManuallyDrop::into_inner(arr)
         }
     }};
+}
+
+/// Array constructor macro.
+///
+/// This macro provides a way to repeat the same macro element multiple times
+/// without requiring `Copy` implementation.
+///
+/// It's possible to define a callback by starting expression with `|` or `move`. As
+/// every closure is it own unique type, it is not possible to have an array of
+/// closures, so this syntax was reused for creating arrays with known indexes.
+///
+/// # Examples
+///
+/// ```
+/// # #[macro_use]
+/// # extern crate array_macro;
+/// # fn main() {
+/// assert_eq!(array!["string"; 3], ["string", "string", "string"]);
+/// assert_eq!(array![|x| x; 3], [0, 1, 2]);
+/// # }
+/// ```
+#[macro_export]
+macro_rules! array {
     [| $($rest:tt)*] => {
-        array![@INTERNAL | $($rest)*]
+        __array_internal![| $($rest)*]
     };
     [move $($rest:tt)*] => {
-        array![@INTERNAL move $($rest)*]
+        __array_internal![move $($rest)*]
     };
     [$expr:expr; $count:expr] => {
         array![|_| $expr; $count]
